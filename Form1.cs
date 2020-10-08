b7c6d4ba2e654a4abadc8c3bfc390c85398bec35
Azazil47 <72367218+Azazil47@users.noreply.github.com>
2020-10-08 18:57:04 +0700

    Добавьте файлы проекта.

---
+++ Form1.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Threading;
+using System.Windows.Forms;
+
+namespace ThreadsTest4
+{
+    public partial class Form1 : Form
+    {
+        Thread[] thread = new Thread[2];
+        public void addCount()
+        {
+            for (int i = 0; i < 100; i++)
+            {
+                Invoke((MethodInvoker)delegate () { textBox1.Text += "1"; });
+
+                Thread.Sleep(500);
+            }
+        }
+        public void addCount2()
+        {
+            for (int i = 0; i < 100; i++)
+            {
+                Invoke((MethodInvoker)delegate () { textBox2.Text += "2"; });
+
+                Thread.Sleep(500);
+            }
+        }
+
+        public Form1()
+        {
+
+
+            InitializeComponent();
+        }
+
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            thread[0] = new Thread(addCount);
+            thread[0].Start();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            thread[1] = new Thread(addCount2);
+            thread[1].Start();
+        }
+
+      }  
+}
---
+++ Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Общие сведения об этой сборке предоставляются следующим набором
+// набора атрибутов. Измените значения этих атрибутов для изменения сведений,
+// связанных со сборкой.
+[assembly: AssemblyTitle("ThreadsTest4")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("ThreadsTest4")]
+[assembly: AssemblyCopyright("Copyright ©  2020")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Установка значения False для параметра ComVisible делает типы в этой сборке невидимыми
+// для компонентов COM. Если необходимо обратиться к типу в этой сборке через
+// COM, следует установить атрибут ComVisible в TRUE для этого типа.
+[assembly: ComVisible(false)]
+
+// Следующий GUID служит для идентификации библиотеки типов, если этот проект будет видимым для COM
+[assembly: Guid("04d847de-6f41-4309-adcb-cd8ced1e2b55")]
+
+// Сведения о версии сборки состоят из указанных ниже четырех значений:
+//
+//      Основной номер версии
+//      Дополнительный номер версии
+//      Номер сборки
+//      Редакция
+//
+// Можно задать все значения или принять номера сборки и редакции по умолчанию 
+// используя "*", как показано ниже:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
